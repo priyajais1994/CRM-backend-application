05c11f51e79821075672fa7553da2ad57e02efc1
priyajais1994 <kumaripriyanka271439@gmail.com>
2023-06-29 17:54:34 +0530

    change db url

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,4 +1,8 @@
 module.exports = {
     DB_Name:"CRM_DB",
-    DB_URL:"mongodb+srv://utkarsh:qwerty123@cluster0.85op4lq.mongodb.net/?retryWrites=true&w=majority"
+
+    DB_URL : "mongodb+srv://priyankajaiswal:13jan2018@cluster0.u3aoeql.mongodb.net/?retryWrites=true&w=majority"
+    
+
+    //DB_URL:"mongodb+srv://utkarsh:qwerty123@cluster0.85op4lq.mongodb.net/?retryWrites=true&w=majority"
 }
